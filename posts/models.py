435b5a987a4d3c3a03f84df3ea9206f1ec236724
Miguel Martin <pomikelor@gmail.com>
2018-11-21 20:47:46 +0100

    Creación del modelo post y las migraciones

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+    PUBLISHED = 'PUB'
+    DRAFT = 'DRA'
+    STATUS = (
+        (PUBLISHED, 'Published'),
+        (DRAFT, 'Draft')
+    )
+
+    title = models.CharField(max_length=150)
+    intro = models.CharField(max_length=300)
+    body = models.TextField()
+    image = models.FileField()
+    status = models.CharField(max_length=3, choices=STATUS, default=DRAFT)
